d563c5bc886e8fe1d30f82df79817ea5c136f018
Nicholas M <nicholasmuturi1@gmail.com>
2020-01-09 16:59:51 +0300

    [chore] created a new component: request-verification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RequestVerificationComponent } from './request-verification/request-verification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RequestVerificationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/request-verification/request-verification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-verification',
+  templateUrl: './request-verification.component.html',
+  styleUrls: ['./request-verification.component.css']
+})
+export class RequestVerificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
