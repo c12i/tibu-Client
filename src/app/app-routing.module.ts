76c40cac631495c4bc834f0887b7d41f7e896ab3
Nicholas M <nicholasmuturi1@gmail.com>
2020-01-13 14:49:12 +0300

    [chore] slightly altered styling of both login & request verification component to make them appear equal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RequestVerificationComponent } from './request-verification/request-verification.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 const routes: Routes = [
+  {path: '', component: LoginPageComponent},
   {path: 'verify', component: RequestVerificationComponent}
 ];
 
