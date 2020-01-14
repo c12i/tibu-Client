664f63412ddefd57f1b9ceca43f2f1b07772e50f
AndyOmondi17 <andyomondi17@gmail.com>
2020-01-14 16:00:17 +0300

    change login routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginPageComponent},
   {path: 'verify', component: RequestVerificationComponent},
   {
