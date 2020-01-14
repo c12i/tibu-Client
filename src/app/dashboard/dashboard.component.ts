3b4ce61fc5137f2919c9a48f529b051dd0c9efe9
Nicholas M <nicholasmuturi1@gmail.com>
2020-01-14 15:52:55 +0300

    [chore] changed dashboard data from static to dynamic, obtained from component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
+
+  request = {
+    patient_name: "John Doe",
+    doctor_name: "Dr. Jane Dutch",
+    specimen: "Urine",
+    patient_age: 30,
+    patient_address: "Straight Outta Compton",
+    symptoms: ["Arms are weak","Knees are heavy"],
+    investigations: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+  }
+
 
 }
