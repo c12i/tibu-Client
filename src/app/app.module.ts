015c9d3805015f13d4a18004b5941a27e26d369c
Nicholas M <nicholasmuturi1@gmail.com>
2020-01-14 18:07:39 +0300

    [IMPORTANT] modified UI colours to company colours

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,13 @@ import { RequestVerificationComponent } from './request-verification/request-ver
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashNavigationComponent } from './dash-navigation/dash-navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-
+import { 
+  MatToolbarModule,MatButtonModule,
+  MatSidenavModule,MatIconModule,
+  MatListModule, MatMenuModule  
+} from '@angular/material';
 
 
 @NgModule({
