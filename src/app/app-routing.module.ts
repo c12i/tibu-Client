29a5b2968f52caf362f2f6e482f786ed6a4c699d
AndyOmondi17 <andyomondi17@gmail.com>
2020-01-16 12:27:46 +0300

    create lab request page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RequestVerificationComponent } from './request-verification/request-ver
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashNavigationComponent } from './dash-navigation/dash-navigation.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableComponent } from './table/table.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginPageComponent},
+  {path:'table', component:TableComponent},
   {path: 'verify', component: RequestVerificationComponent},
   {
     path: 'dashboard', 
