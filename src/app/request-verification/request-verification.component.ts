4299ab1cd4d66f700d38e3bf89d6e2e2fc21e91a
Nicholas M <nicholasmuturi1@gmail.com>
2020-01-16 10:56:07 +0300

    [feat] Request id can be used to obtain request details, Interceptor tested and now confirmed it works

--- src/app/request-verification/request-verification.component.ts
+++ src/app/request-verification/request-verification.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-request-verification',
